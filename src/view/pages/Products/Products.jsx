b5d3c81a2ce928497a2ea07f277acd1d685df15d
Sunidhi <sunidhi@trigvent.com>
2024-10-14 11:15:51 +0530

    product-commit3

--- src/view/pages/Products/Products.jsx
+++ src/view/pages/Products/Products.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import useProducts from "../../../hooks/useProducts";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
+import Grid from "@mui/material/Grid2";
 import Typography from "@mui/material/Typography";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -26,74 +26,120 @@ function Products() {
 
   return (
     <>
-      {/* Top bar of Products Page starts*/}
-      <Stack direction="row" spacing={1} mt={2}>
-        {/* Search Box */}
-        <Box
-          component="form"
-          sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField id="outlined-basic" label="Search" variant="outlined" />
-        </Box>
-        {/* Search Box Ends*/}
-
-        {/* Icons Display */}
-        <GridViewIcon></GridViewIcon>
-        <MenuIcon></MenuIcon>
-
-        {/* Products Counting */}
-        <Typography>Total Products</Typography>
-
-        {/* Select Box */}
-        <Box sx={{ padding: 2 }}>
-          <FormControl sx={{ Width: 12 }}>
-            {/* <InputLabel id="product-select-label">Select Value</InputLabel> */}
-            <Select
-              labelId="product-select-label"
-              id="product-select"
-              value={selectedValue}
-              label="Select Value"
-              onChange={handleSelectChange}
-            >
-              <MenuItem value={30}>Price(lowest)</MenuItem>
-              <MenuItem value={30}>Price(highest)</MenuItem>
-              <MenuItem value={10}>Price(a-z)</MenuItem>
-              <MenuItem value={20}>Price(z-a)</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-        {/* Select Box Ends  */}
-      </Stack>
-      {/* Top bar of Products Page Ends*/}
-
-      {/* Box to display all products */}
-      <Box sx={{ flexGrow: 1, padding: 2 }}>
-        <Grid container spacing={2}>
-          {products?.map((product) => (
-            <Grid item xs={12} sm={6} md={4} key={product?.id}>
-              <Box
-                textAlign="center"
-                border={1}
-                borderColor="grey.300"
-                borderRadius={2}
-                p={2}
-              >
-                <img
-                  src={product?.image}
-                  alt={product?.name}
-                  style={{ width: "100%", height: "200px", objectFit: "cover" }}
-                />
-                <Box mt={1}>
-                  <Typography variant="h6">{product?.name}</Typography>
-                  <Typography variant="body2">${product?.price}</Typography>
+      {/* Outer Grid starts*/}
+      <Grid container direction="row" spacing={2}>
+        <Grid item sm={6}>
+          {/* Sidebar Grid Starts */}
+          <Grid container border={"1px solid red"}>
+            <Grid item xs={12} sm={6}>
+              {/* Sidebar starts*/}
+              {/* Search Box */}
+              <Box>
+                <Box
+                  component="form"
+                  sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <TextField
+                    id="outlined-basic"
+                    label="Search"
+                    variant="outlined"
+                  />
                 </Box>
+                {/* Search Box Ends*/}
+                <Typography>Catergory</Typography>
               </Box>
             </Grid>
-          ))}
+            {/* Sidebar ends*/}
+          </Grid>
+          {/* Sidebar Grid ends */}
         </Grid>
-      </Box>
+
+        <Grid item sm={6}>
+          {/* Topbar+Center Grid starts */}
+          <Grid container border={"1px solid red"} mt={2}>
+            <Grid container border={"1px solid blue"} mt={2}>
+              <Grid item xs={6}>
+                {/* Top bar of Products Page starts*/}
+                <Stack direction="row" spacing={1} mt={2}>
+                  {/* Icons Display */}
+                  <GridViewIcon></GridViewIcon>
+                  <MenuIcon></MenuIcon>
+
+                  {/* Products Counting */}
+                  <Typography>Total Products</Typography>
+
+                  {/* Select Box */}
+                  <Box sx={{ padding: 2 }}>
+                    <FormControl sx={{ Width: 12 }}>
+                      {/* <InputLabel id="product-select-label">Select Value</InputLabel> */}
+                      <Select
+                        labelId="product-select-label"
+                        id="product-select"
+                        value={selectedValue}
+                        label="Select Value"
+                        onChange={handleSelectChange}
+                      >
+                        <MenuItem value={30}>Price(lowest)</MenuItem>
+                        <MenuItem value={30}>Price(highest)</MenuItem>
+                        <MenuItem value={10}>Price(a-z)</MenuItem>
+                        <MenuItem value={20}>Price(z-a)</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                  {/* Select Box Ends  */}
+                </Stack>
+              </Grid>
+            </Grid>
+            {/* Top bar of Products Page Ends*/}
+
+            {/* Center Box starts */}
+            <Grid container border={"1px solid green"} mt={2}>
+              <Grid item xs={6}>
+                {/* Box to display all products */}
+                <Box sx={{ flexGrow: 1, padding: 2 }}>
+                  <Grid container spacing={2}>
+                    {products?.map((product) => (
+                      <Grid item xs={12} sm={6} md={4} key={product?.id}>
+                        <Box
+                          textAlign="center"
+                          border={1}
+                          borderColor="grey.300"
+                          borderRadius={2}
+                          p={2}
+                        >
+                          <img
+                            src={product?.image}
+                            alt={product?.name}
+                            style={{
+                              width: "100%",
+                              height: "200px",
+                              objectFit: "cover",
+                            }}
+                          />
+                          <Box mt={1}>
+                            <Typography variant="h6">
+                              {product?.name}
+                            </Typography>
+                            <Typography variant="body2">
+                              ${product?.price}
+                            </Typography>
+                          </Box>
+                        </Box>
+                      </Grid>
+                    ))}
+                  </Grid>
+                </Box>
+              </Grid>
+            </Grid>
+            {/* Center Box ends */}
+
+            {/* Topbar+Center Grid ends */}
+          </Grid>
+        </Grid>
+        {/* Outer Grid ends*/}
+      </Grid>
     </>
   );
 }
