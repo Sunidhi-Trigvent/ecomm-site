c5639ca7ab0dfbc85e1255a30923429f9e2194d7
Sunidhi <sunidhi@trigvent.com>
2024-10-29 16:18:07 +0530

    on addToCart Show Modal

--- src/view/pages/product/designing.jsx/container.jsx
+++ src/view/pages/product/designing.jsx/container.jsx
@@ -11,6 +11,12 @@ import { useSelector } from "react-redux"; // For getting the user ID from Redux
 import fullStar from "../../../../assets/images/full_star.png";
 import emptyStar from "../../../../assets/images/half_star.png";
 import { useSnackbar } from "../../../../components/Snackbar";
+import { useModal } from "../../../../components/Modal";
+import Login from "../../../pages/Login/login";
+import * as Yup from "yup";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../../../redux/userSlice"; // Adjust the path as needed
+import useUser from "../../../../hooks/useUser";
 
 export default function Container_main({ productDetail }) {
   const navigate = useNavigate();
@@ -21,6 +27,9 @@ export default function Container_main({ productDetail }) {
   const [amount, setAmount] = React.useState(1);
   const [selectedImage, setSelectedImage] = React.useState(null);
   const { showSnackbar } = useSnackbar();
+  const { showModal, closeModal } = useModal();
+  const dispatch = useDispatch();
+  const { userLogin } = useUser(); // Assuming this handles the login
 
   const setDecrease = () => {
     amount > 1 ? setAmount(amount - 1) : setAmount(1);
@@ -58,9 +67,45 @@ export default function Container_main({ productDetail }) {
     return `rgba(${r}, ${g}, ${b}, 0.5)`;
   };
 
+  // Function to handle form submission
+  const handleLogin = async (values) => {
+    const { email, password } = values;
+
+    try {
+      const response = await userLogin({ email, password });
+      const { token, data } = response;
+
+      // Store token in local storage
+      localStorage.setItem("token", token);
+      // Dispatch user data to Redux store
+      dispatch(setUser(data));
+
+      // Redirect user to the dashboard or home page on successful login
+      closeModal();
+      navigate("/");
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
+
   const handleAddToCart = async () => {
     if (!userId) {
       showSnackbar("Login First", "error");
+
+      const loginValidationSchema = Yup.object({
+        email: Yup.string().required(),
+        password: Yup.string().required(),
+      });
+
+      //open login page-
+      showModal({
+        title: "Login",
+        content: <Login />,
+        onSubmit: handleLogin,
+        confirmText: "Sign-In",
+        cancelText: "Cancel",
+        validation: loginValidationSchema,
+      });
     }
     try {
       const cartData = {
