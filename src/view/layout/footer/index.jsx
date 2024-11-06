8f795fb967452b42fc0e0ea31ffb2d059c1196a5
Sunidhi <sunidhi@trigvent.com>
2024-11-06 15:58:30 +0530

    Making Footer-4

--- src/view/layout/footer/index.jsx
+++ src/view/layout/footer/index.jsx
@@ -7,25 +7,32 @@ function Footer() {
       <div>Footer</div>
       <Box
         sx={{
-          bgcolor: "#0A2F54",
-          width: "100%",
-          height: "180px",
-          position: "relative",
+          mt: "60px",
         }}
       >
-        {/* Secondary box positioned halfway above the main box */}
         <Box
           sx={{
-            bgcolor: "lightgray",
-            width: "80%", // Adjust the width to be less than the main box
-            height: "90px",
-            position: "absolute",
-            bottom: "50%", // Position it halfway above
-            left: "50%",
-            transform: "translateX(-50%)", // Center horizontally
-            borderRadius: "8px", // Optional: Add some border radius
+            bgcolor: "#0A2F54",
+            width: "100%",
+            height: "180px",
+            position: "relative",
           }}
-        ></Box>
+        >
+          {/* Secondary box positioned halfway above the main box */}
+          <Box
+            sx={{
+              bgcolor: "lightgray",
+              width: "80%", // Adjust the width to be less than the main box
+              height: "90px",
+              position: "absolute",
+              bottom: "50%", // Position it halfway above
+              top: "-20%",
+              left: "50%",
+              transform: "translateX(-50%)", // Center horizontally
+              borderRadius: "8px", // Optional: Add some border radius
+            }}
+          ></Box>
+        </Box>
       </Box>
     </>
   );
