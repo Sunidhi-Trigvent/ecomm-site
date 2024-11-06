767a8103bed86c71b525cc8058182ff22fabf401
Sunidhi <sunidhi@trigvent.com>
2024-11-06 17:29:42 +0530

    Footer3

--- src/view/layout/footer/GridFooter.jsx
+++ src/view/layout/footer/GridFooter.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Grid from "@mui/material/Grid2";
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const GridFooter = () => {
   return (
     <>
       <Grid container>
-        <Grid item size={4}></Grid>
-        <Grid item size={1}>
+        <Grid item size={2}></Grid>
+        <Grid item size={2}>
           <Typography sx={{ color: "white" }}>
             Amazon Store
             <br />
@@ -15,7 +24,7 @@ const GridFooter = () => {
             consectetur adipisicing elit.
           </Typography>
         </Grid>
-        <Grid item size={1}>
+        <Grid item size={2}>
           <Typography sx={{ color: "white" }}>
             This is an ecommerce
             <br />
@@ -34,9 +43,21 @@ const GridFooter = () => {
           </Stack>
         </Grid>
 
-        <Grid item size={1}></Grid>
-        <Grid item size={1}></Grid>
-        <Grid item size={4}></Grid>
+        <Grid item size={2}>
+          <Typography sx={{ color: "white" }}>Follow Us</Typography>
+          <IconButton sx={{ color: "white" }}>
+            <FacebookIcon />
+            <InstagramIcon />
+            <YouTubeIcon />
+          </IconButton>
+        </Grid>
+        <Grid item size={2}>
+          <Typography sx={{ color: "white" }}>
+            Call Us
+            <br /> +91 123456789
+          </Typography>
+        </Grid>
+        <Grid item size={2}></Grid>
       </Grid>
     </>
   );
